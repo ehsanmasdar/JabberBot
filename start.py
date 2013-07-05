ef4b57313c9890fd9e27db389c22db7604579d0a
cyron <cyron@vm-3.cyron.koding.kd.io>
2013-07-05 03:48:36 +0000

    Inital commit, updated base for bot

---
+++ start.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Run this script under screen (man screen)
+# It will restart neutron over and over again.
+# And plz, do not forget to set AUTO_RESTART to 0 in config.txt file.
+
+from os import system
+from sys import exit as sys_exit
+
+system("python ./bot.py")
+
